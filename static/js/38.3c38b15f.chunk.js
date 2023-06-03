3ddbaeb51997e621a46bda38d62c77e895961665
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-06-03 11:58:47 +0000

    Deploying to gh-pages from @ BieliakovS/goit-react-hw-05-movies@c093a904f9a808970300e793cc1cbc41cc1aa398 🚀

---
+++ static/js/38.3c38b15f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[38],{38:function(e,t,n){n.r(t);n(791);var r=n(184);t.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"404 - Page not found"})})}}}]);
+//# sourceMappingURL=38.3c38b15f.chunk.js.map
